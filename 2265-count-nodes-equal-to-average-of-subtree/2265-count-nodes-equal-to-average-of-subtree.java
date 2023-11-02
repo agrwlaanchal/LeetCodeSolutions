664e6a5a63c70cdd9c806f6670398958af96215f
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2023-11-02 09:41:11 +0530

    Time: 17 ms (13.34%), Space: 42.6 MB (93.99%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     int res = 0; 
+    public int averageOfSubtree(TreeNode root) {
+     
+     
+        
+        
+     
+        
+            if(root!=null){
+                
+                if( root.left!=null){
+                    averageOfSubtree(root.left);
+                }
+                
+            int leftnodes = getSize(root.left); 
+            int leftsum = getSum(root.left);
+           
+         //   System.out.println(root.val+" " +leftnodes+ " "+leftsum);
+            int rightnodes = getSize(root.right);
+            int rightsum = getSum(root.right);
+            
+            if( (leftsum+rightsum+root.val)/(leftnodes+rightnodes+1) == root.val ){
+                res++;
+            }
+               
+                if(root.right!=null){
+            averageOfSubtree(root.right);
+                }
+            
+        }
+            return res; 
+        
+    }
+    
+    
+    public int getSize(TreeNode root){
+        if(root==null)
+            return 0;
+        
+        return 1+getSize(root.left)+getSize(root.right);
+    }
+    
+    public int getSum(TreeNode root){
+        
+        if(root==null)
+            return 0; 
+        
+            return root.val+getSum(root.left)+getSum(root.right);
+            
+        
+    }
+    
+}
