662ce7898b05e6b2af3cf24286945343fee1a1ab
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2023-11-11 20:08:18 +0530

    Time: 125 ms (62.96%), Space: 54.5 MB (77.78%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.java
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.java
@@ -22,39 +22,6 @@ class Graph {
     
     public int shortestPath(int node1, int node2) {
      
-       
-        
-        /*
-        int[] dist=new int[V];int res=0; 
-    	Arrays.fill(dist,Integer.MAX_VALUE);dist[src]=0;
-    	boolean[] fin=new boolean[V]; 
-    
-    	for (int count = 0; count < V-1 ; count++) 
-    	{ 
-    		int u = -1; 
-    
-    		for(int i=0;i<V;i++)
-    		    if(!fin[i]&&(u==-1||dist[i]<dist[u]))
-    		        u=i;
-    		fin[u] = true; 
-    		
-    		for (int v = 0; v < V; v++) 
-    
-    			if (graph[u][v]!=0 && fin[v] == false) 
-    				dist[v] = Math.min(dist[v],dist[u]+graph[u][v]); 
-    	} 
-        return dist;
-        */
-        
-        
-        
-        /*for(int i=0;i<g.length;i++){
-            for(int j=0; j<g[i].length;j++){
-                System.out.print(g[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println(node1 +" "+node2);*/
         int V = g.length;
         int []dist = new int[V];
         Arrays.fill(dist, Integer.MAX_VALUE);
@@ -71,15 +38,10 @@ class Graph {
                 }
             }
             
-             //   System.out.println("u is "+u);
                 visited[u]  = true;
                 for(int v=0 ; v<V ; v++){
-               //     System.out.println("v is "+v);
                     if(!visited[v] && g[u][v]!=0 ){
-                 //       System.out.println(u +" "+v+ " "+dist[v] + " "+dist[u] +" "+g[u][v]);
                         dist[v] = Math.min(dist[v], dist[u] + g[u][v]);
-                       // if(v==node2)
-                        //    break;
                     }
                 }
             }
