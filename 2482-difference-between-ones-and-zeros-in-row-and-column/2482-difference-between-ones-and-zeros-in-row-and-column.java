b468a29c6d369e5df58de676d8f6675a92aa2863
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2023-11-24 22:34:43 +0530

    Time: 12 ms (48.38%), Space: 75.3 MB (31.27%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
@@ -6,7 +6,6 @@ class Solution {
         // for each row, count 1s
         
         int [] rows1 = new int[grid.length];
-        int [] rows0 = new int[grid.length];
         for(int i=0; i<grid.length ;i++){
             int count =0 ;
             for(int j=0 ;j<grid[i].length ;j++){
@@ -14,7 +13,6 @@ class Solution {
                     count++;
                 }
                 rows1[i] = count; 
-                rows0[i] = grid.length-count;
             }
             
         }
@@ -22,7 +20,6 @@ class Solution {
      //   System.out.println(Arrays.toString(rows1));
       //   System.out.println(Arrays.toString(rows0));
         int []col1 = new int[grid[0].length];
-        int []col0 = new int[grid[0].length];
         
         
         for(int j=0;j<grid[0].length ;j++){
@@ -36,7 +33,7 @@ class Solution {
             }
             
             col1[j] = count;
-            col0[j] = grid[0].length-count;
+        
             
         }
         
@@ -49,7 +46,7 @@ class Solution {
         for(int i=0 ;i<res.length;i++){
             for(int j=0; j<res[i].length ;j++){
                 
-                res[i][j] = rows1[i] + col1[j] -rows0[i]-col0[j];
+                res[i][j] = 2*rows1[i] + 2*col1[j] -grid.length - grid[0].length;
                 
             }
         }
