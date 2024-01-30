beca03afefa7908376a3673127fd4f098a3550a3
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-01-30 21:11:19 +0530

    Time: 47 ms (14.12%), Space: 45.2 MB (37.18%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,38 +1,52 @@
 class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        
+     
         List<Boolean> res = new ArrayList<>();
-        for(int i=0;i<l.length;i++){
+        int m = l.length;
+        
+        for(int i=0;i<m; i++){
             
+           ArrayList<Integer> arr = new ArrayList<>();
             
-            int start = l[i];
-            int end = r[i];
            
-            int []temp = new int[end-start+1];
-            boolean flag=true;
-            int index =0 ;
-            for(int j=start;j<=end;j++){
-                temp[index] = nums[j];
-                index++;
-            }
-            Arrays.sort(temp);
-       //     System.out.println(Arrays.toString(temp));
-            int diff = temp[1]-temp[0];
-            for(int j=2; j<temp.length;j++){
-                if( (temp[j]-temp[j-1]) !=diff){
+                
+                int start = l[i];
+                int end = r[i];
+           
+          /*  if(end-start<2)
+            {
+                res.add(false);
+            //    System.out.println("adding here first");
+                continue;
+            }*/
+            
+             for(int j=start; j<=end; j++){
+                 arr.add(nums[j]);
+             }
+            
+            Collections.sort(arr);
+            
+            if(arr.size()<2)
+                res.add(false);
+            
+           int diff = arr.get(1)-arr.get(0);
+            int flag=0;
+            for(int j=2;j<arr.size();j++){
+                if(arr.get(j)-arr.get(j-1)!=diff){
                     res.add(false);
-                    flag=false;
+                    flag=1;
+                //    System.out.println("adding here second");
                     break;
-                } 
+                }
             }
-            
-            if(flag==true)
+            if(flag==0)
             res.add(true);
+            //    System.out.println("adding here third");
             
-           
-           
             
         }
+        
         return res;
+        
     }
 }
