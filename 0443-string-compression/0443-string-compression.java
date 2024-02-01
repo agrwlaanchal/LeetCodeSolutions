93823f249cc3245cfcda39fa27ab33added09ecc
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-02-01 18:40:44 +0530

    Time: 1 ms (99.54%), Space: 43.9 MB (54.39%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -7,7 +7,7 @@ class Solution {
             return 1;
        
         int count =1 ;   
-        String s = ""+chars[0];
+      //  String s = ""+chars[0];
         char curr = chars[0];
         int index =1;
         
@@ -16,22 +16,18 @@ class Solution {
             if(chars[i]==curr){
                 count++;
             } else{
-             //   System.out.println("count is"+count);
                 if(count>1){
                     String temp =String.valueOf(count);
                     for(int j=0;j<temp.length();j++){
                         chars[index++]=temp.charAt(j);
                     }
-                     s=s+count;
+            //         s=s+count;
                 }
                
-            //     System.out.println("s is"+s);
                 count=1;
-                s=s+chars[i];
+            //    s=s+chars[i];
                 chars[index++]=chars[i];
-          //       System.out.println("s now is"+s);
                 curr=chars[i];
-         //        System.out.println("curr is"+curr);
             }
             
         }
@@ -41,12 +37,11 @@ class Solution {
                     for(int j=0;j<temp.length();j++){
                         chars[index++]=temp.charAt(j);
                     }
-            s=s+count;
+          //  s=s+count;
                 }
         
-         
-       //    System.out.println("s final is"+s);
-        return s.length();
+         return index;
+       // return s.length();
         
     }
 }
