ecc178e52e1f70e5a0b6c8b53e74937a114bd20c
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-02-01 18:59:42 +0530

    Time: 71 ms (60.22%), Space: 45.5 MB (76.19%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -7,9 +7,9 @@ class Solution {
             
             if(s.charAt(i)=='*'){
                 
-                if(!st.isEmpty()){
+               // if(!st.isEmpty()){
                     st.pop();
-                }
+                //}
                 
             }else{
                 st.push(s.charAt(i));
@@ -24,20 +24,10 @@ class Solution {
         
         for(int i=arr.length-1; i>=0;i--){
             arr[i]=st.pop();
-        //    System.out.println(arr[i]);
         }
-     /*   while(!st.isEmpty()){
-            temp=temp+st.pop();
-        }*/
         
         return new String(arr);
         
-    /*    String res="";
-        for(int i=temp.length()-1;i>=0;i--){
-            res=res+temp.charAt(i);
-        }
-        return res; 
-     */   
         
     }
 }
