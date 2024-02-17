88aeb3489e93b69b34606f916309f26dc619657f
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-02-17 15:46:03 +0530

    Time: 7 ms (76.24%) | Memory: 45.2 MB (94.48%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -17,54 +17,113 @@ class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
 
        Queue<int[]> pq = new LinkedList<>();
-        int[] arr = new int[3];
-        arr[0] = entrance[0];
-        arr[1] = entrance[1];
-        arr[2] = 0;
-        pq.offer(arr);
-        maze[entrance[0]][entrance[1]] = '+';
+        int dis = 1;
+        int i = entrance[0];
+        int j = entrance[1];
+        maze[i][j] = '+';
+        if (issafe(i + 1, j, maze)) {
+            int[] temp = new int[3];
+            temp[0] = i + 1;
+            temp[1] = j;
+            temp[2] = 1;
+            pq.add(temp);
+            maze[i + 1][j] = '+';
+        }
+
+        if (issafe(i - 1, j, maze)) {
+            int[] temp = new int[3];
+            temp[0] = i - 1;
+            temp[1] = j;
+            temp[2] = 1;
+            pq.add(temp);
+            maze[i - 1][j] = '+';
+        }
+
+        if (issafe(i, j + 1, maze)) {
+            int[] temp = new int[3];
+            temp[0] = i;
+            temp[1] = j + 1;
+            temp[2] = 1;
+            pq.add(temp);
+            maze[i][j + 1] = '+';
+        }
+        if (issafe(i, j - 1, maze)) {
+            int[] temp = new int[3];
+            temp[0] = i;
+            temp[1] = j - 1;
+            temp[2] = 1;
+            pq.add(temp);
+            maze[i][j - 1] = '+';
+
+        }
 
         if (pq.isEmpty()) {
             return -1;
         }
 
-        for (int k = 0; k < maze.length; k++) {
-            for (int t = 0; t < maze[0].length; t++) {
-                System.out.print(maze[k][t] + " ");
-            }
-            System.out.println();
-        }
-
-        // int res = Integer.MAX_VALUE;
-
-        int[][] dirs = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-
+        /*
+         * for(int k=0;k<maze.length;k++){
+         * for(int t=0;t<maze[0].length;t++){
+         * System.out.print(maze[k][t]+" ");
+         * }
+         * System.out.println();
+         * }
+         */
+        int res = Integer.MAX_VALUE;
         while (!pq.isEmpty()) {
+            int size = pq.size();
+            for (int k = 0; k < size; k++) {
+                int[] p = pq.poll();
 
-            int[] p = pq.poll();
-            int curri = p[0];
-            int currj = p[1];
+                if (atboundry(p, maze)) {
+                   return p[2];
+                } else {
+                    if (issafe(p[0] + 1, p[1], maze)) {
+                        int[] temp = new int[3];
+                        temp[0] = p[0] + 1;
+                        temp[1] = p[1];
+                        temp[2] = p[2] + 1;
+                        pq.add(temp);
+                        maze[p[0] + 1][p[1]] = '+';
 
-            for (int[] dir : dirs) {
+                    }
+                    if (issafe(p[0] - 1, p[1], maze)) {
+                        int[] temp = new int[3];
+                        temp[0] = p[0] - 1;
+                        temp[1] = p[1];
+                        temp[2] = p[2] + 1;
+                        pq.add(temp);
+                        maze[p[0] - 1][p[1]] = '+';
 
-                int nextrow = curri + dir[0];
-                int nextcol = currj + dir[1];
-
-                if (issafe(nextrow, nextcol, maze)) {
-
-                    if (atboundry(nextrow, nextcol, maze)) {
-                        return p[2] + 1;
                     }
 
-                    maze[nextrow][nextcol] = '+';
-                    pq.offer(new int[] { nextrow, nextcol, p[2] + 1 });
+                    if (issafe(p[0], p[1] + 1, maze)) {
+                        int[] temp = new int[3];
+                        temp[0] = p[0];
+                        temp[1] = p[1] + 1;
+                        temp[2] = p[2] + 1;
+                        pq.add(temp);
+                        maze[p[0]][p[1] + 1] = '+';
 
+                    }
+                    if (issafe(p[0], p[1] - 1, maze)) {
+                        int[] temp = new int[3];
+                        temp[0] = p[0];
+                        temp[1] = p[1] - 1;
+                        temp[2] = p[2] + 1;
+                        pq.add(temp);
+                        maze[p[0]][p[1] - 1] = '+';
+
+                    }
                 }
 
             }
+
         }
 
+        if(res==Integer.MAX_VALUE)
         return -1;
+        return res;
 
     }
 
@@ -74,10 +133,10 @@ class Solution {
 
     }
 
-    public boolean atboundry(int i, int j, char[][] maze) {
+    public boolean atboundry(int[] p, char[][] maze) {
         // System.out.println(p[0]+ " "+p[1]+" "+(maze.length-1)+"
         // "+(maze[0].length-1));
-        if (i == 0 || i == maze.length - 1 || j == 0 || j == maze[0].length - 1) {
+        if (p[0] == 0 || p[0] == maze.length - 1 || p[1] == 0 || p[1] == maze[0].length - 1) {
             return true;
         }
         return false;
