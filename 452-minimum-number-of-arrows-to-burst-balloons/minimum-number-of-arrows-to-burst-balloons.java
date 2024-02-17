30ffc730f623dda706f78a9923b84ca33d2d2933
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-02-17 13:01:51 +0530

    Time: 60 ms (27.17%) | Memory: 68.5 MB (89.66%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -0,0 +1,72 @@
+class interval{
+    int start;
+    int end;
+}
+
+
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+         if(points.length==0)
+            return 0;
+        
+        HashSet<int[]> hset = new HashSet<>();
+        Arrays.sort(points, new Comparator<int[]>(){
+            
+        
+            public int compare(final int []a1, final int []a2){
+                
+                if (a1[0] > a2[0]) 
+                return 1; 
+            else
+                return -1; 
+                
+            }
+            
+        });
+        
+       /* for(int i=0; i<points.length; i++){
+            
+                System.out.println(points[i][0]+","+points[i][1]);
+            
+        }
+
+System.out.println("--------------------");*/
+         int res=0;
+         
+            
+        for(int i=1;i<points.length;i++){
+            
+            if(points[i][0] <= points[i-1][1]){
+                //overlapping
+                points[i][0]= points[i-1][0];
+                points[i][1] = Math.min(points[i-1][1] , points[i][1]);
+                points[i-1][1] =points[i][1];
+                
+               
+                
+            }
+            
+              
+        }
+
+      /*   for(int i=0; i<points.length; i++){
+            
+                System.out.println(points[i][0]+","+points[i][1]);
+            
+        }*/
+
+         int count=1;
+         for(int i=1;i<points.length;i++)
+         {
+             if(points[i][0]!=points[i-1][0] && points[i][1]!=points[i-1][1])
+                 count++;
+                 
+                 
+         }   
+        
+        return count;
+
+  
+
+    }
+}
