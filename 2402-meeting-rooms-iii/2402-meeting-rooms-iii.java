4efcf95d9ba0fa70e006ed56ff8ca28ba89304ce
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-02-18 16:54:49 +0530

    Time: 70 ms (80.12%), Space: 101.1 MB (17.10%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -5,57 +5,61 @@ class Solution {
         // Sort meetings by start time
         Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
 
-        int[] arr = new int[n];
+        
+        int[]arr = new int[n];
         long[] endtime = new long[n];
         Arrays.fill(arr, 0);
-
-        long mintime = Integer.MAX_VALUE;
-        int room = -1;
-
-    outer: 
-        for (int i = 0; i < meetings.length; i++) {
-            mintime = endtime[0];
-            room = 0;
-
-            for (int j = 0; j < n; j++) {
-                if (arr[j] != 0) {
-                    if (endtime[j] <= meetings[i][0]) {
-                        arr[j] = arr[j] + 1;
-                        endtime[j] = meetings[i][1];
-                        continue outer;
-                    } else {
-                        if (mintime > endtime[j]) {
-                            mintime = endtime[j];
-                            room = j;
-                        }
-                    }
-                } else if (arr[j] == 0) {
-                    arr[j] = 1;
-                    endtime[j] = meetings[i][1];
+          int room =-1;
+     outer:   for(int i=0 ;i<meetings.length;i++){
+            
+       long mintime = endtime[0];
+      room =0;
+            
+            for(int j=0;j<n;j++){
+               if(arr[j]!=0){
+                
+                if(endtime[j]<=meetings[i][0]){
+                     arr[j]=arr[j]+1;
+                     endtime[j]=meetings[i][1];
+               //       System.out.println(Arrays.toString(arr));
+                //      System.out.println(Arrays.toString(endtime));
                     continue outer;
+                }else{
+                    if(mintime>endtime[j]){
+                        mintime=endtime[j];
+                        room=j;
+                    }
                 }
+                
+            }else if(arr[j]==0){
+                arr[j]=1;
+                endtime[j]=meetings[i][1];
+             //        System.out.println(Arrays.toString(arr));
+              //       System.out.println(Arrays.toString(endtime));
+                continue outer;
+            } 
             }
-
-            if (room == -1) {
-                // If all rooms are occupied, delay the meeting
-                // You can handle this case based on your requirements
-                // For now, let's print a message and continue to the next meeting
-                System.out.println("All rooms are occupied. Meeting delayed.");
-                continue;
-            }
-
-            arr[room] = arr[room] + 1;
-            endtime[room] = endtime[room] + (meetings[i][1] - meetings[i][0]);
+            
+            
+            arr[room]=arr[room]+1;
+            endtime[room] = endtime[room]+(meetings[i][1]-meetings[i][0]);
+            
+      //     System.out.println(Arrays.toString(arr));
+       //    System.out.println(Arrays.toString(endtime));
+            
         }
-
-        int max = 0;
-        int in = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
+        
+        int max =0 ;
+        int in =0;
+        for(int i=0;i<arr.length;i++){
+            
+            if(arr[i]>max){
                 max = arr[i];
-                in = i;
+                in=i;
             }
         }
-        return in;
-    }
+        return in; 
+        
+        
+         }
 }
