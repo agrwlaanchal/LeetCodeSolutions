bb831f4a36bb3da47ca06cbff99d32b1ee9961c6
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-02-24 12:02:14 +0530

    Time: 9 ms (27.51%), Space: 45.7 MB (9.00%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+
+      TreeMap<Integer,List<Integer>> hmap = new TreeMap<>();
+
+        for(int i=0 ;i<groupSizes.length;i++){
+
+            if(hmap.containsKey(groupSizes[i])){
+                List<Integer> temp = hmap.get(groupSizes[i]);
+                temp.add(i);
+                hmap.put(groupSizes[i], temp);
+            }else{
+                List<Integer> temp = new ArrayList<>();
+                temp.add(i);
+                hmap.put(groupSizes[i], temp);
+            }
+        }
+
+ 
+
+
+    List<List<Integer>> res=new ArrayList<>();
+        for(Map.Entry<Integer, List<Integer>> entry : hmap.entrySet()){
+
+            int gsize = entry.getKey();
+            List<Integer> temp = entry.getValue();
+             
+             
+             int i=0;
+             while(i<temp.size()){
+                    List<Integer> r = new ArrayList<>();
+                 for(int j=0;j<gsize; j++){
+                     if(i<temp.size()){
+                        r.add(temp.get(i));
+                        i++;
+                     }
+                 }
+                 res.add(r); 
+
+             }
+           
+
+        }
+        return res; 
+    }
+}
