82e7a6845e15853b7b3867f67fb6736c12c87f3a
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-02-26 19:44:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (26.25%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        if(list1==null && list2==null)
+        return null;
+
+        if(list1==null)
+        {
+            return list2;
+        }
+
+        if(list2==null){
+            return list1;
+        }
+
+
+        ListNode curr = null;
+        ListNode result = null; 
+
+        while(list1!=null && list2!=null){
+
+            if(list1.val < list2.val){
+                if(result==null){
+                    curr= new ListNode(list1.val);
+                    result = curr; 
+                }else{
+                    curr.next = new ListNode(list1.val);
+                    curr=curr.next;
+                }
+                list1=list1.next;
+            }else{
+                if(result==null){
+                    curr= new ListNode(list2.val);
+                    result = curr; 
+                }else{
+                    curr.next = new ListNode(list2.val);
+                    curr=curr.next;
+                }
+                list2=list2.next;
+            }
+
+        }
+
+
+        while(list1!=null){
+              if(result==null){
+                    curr= new ListNode(list1.val);
+                    result = curr; 
+                }else{
+                    curr.next = new ListNode(list1.val);
+                    curr=curr.next;
+                }
+                list1=list1.next;
+        }
+
+         while(list2!=null){
+              if(result==null){
+                    curr= new ListNode(list2.val);
+                    result = curr; 
+                }else{
+                    curr.next = new ListNode(list2.val);
+                    curr=curr.next;
+                }
+                list2=list2.next;
+        }
+
+        return result; 
+
+    }
+}
