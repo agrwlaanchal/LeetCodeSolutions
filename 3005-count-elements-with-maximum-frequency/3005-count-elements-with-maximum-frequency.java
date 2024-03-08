0c3846b0e78cef882998ba5d46f32f2ecc8321d9
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-03-08 09:45:08 +0530

    Time: 3 ms (31.20%), Space: 41.8 MB (94.19%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -5,12 +5,9 @@ class Solution {
         int count =0 ;
         
         for(int i=0; i<nums.length ; i++){
-            if(hmap.containsKey(nums[i])){
-                hmap.put(nums[i], hmap.get(nums[i])+1);
-            }else{
-                hmap.put(nums[i],1);
-            }
             
+            hmap.put(nums[i], hmap.getOrDefault(nums[i], 0)+1);
+         
             if(hmap.get(nums[i])>count){
                 count=hmap.get(nums[i]);
             }
