803257b3a125985c662c3af828bc58a3fd60fdb8
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-03-09 11:42:03 +0530

    Time: 1 ms (96.67%), Space: 62.2 MB (92.22%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -3,22 +3,19 @@ class Solution {
      
         //traverse the arrays until we get a common
         // move the smaller values in array 
-        // keep initial res as -1;
-        
-        
-        
+        // return -1 if nothing is common     
         
        int index1 =0; 
         int index2 = 0;
         
         while(index1<nums1.length && index2<nums2.length){
             
-            if(nums1[index1]==nums2[index2]){
-                return nums1[index1];
-            }else if(nums1[index1]<nums2[index2]){
+             if(nums1[index1]<nums2[index2]){
                 index1++;
-            }else{
+            }else if(nums1[index1]>nums2[index2]){
                 index2++;
+            }else{
+                return nums1[index1];
             }
             
         }
