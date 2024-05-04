80897c6f373a484cc01b247ad1e062058d2981a6
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-05-04 13:56:07 +0530

    Time: 16 ms (97.17%), Space: 54.8 MB (36.65%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -9,46 +9,18 @@ class Solution {
           int end = people.length-1;
           
       
-        //2 2 8 8 11
-        while(start<people.length ){
+        
+        
+        while(start<=end){
             
-            
-            int oldstart  = start;
-            
-            if(people[start]<0){
+           if((people[start]+people[end])<=limit){
                 start++;
-                continue;
-            }
-            
-            
-            while(start<end){
-         //       System.out.println("start "+start+" end "+end);
-                
-            if(people[start]>0 && people[end]>0 &&  people[start]+people[end]<=limit){
-       //         System.out.println("going here "+start+" "+end);
-                count++;
-                people[start]=-1;
-                people[end]=-1;
-                start++;
-                break;
-            }else{
-                end--;
-            }
-            }
-            
-           // System.out.println("-------");
-            
-            if(oldstart ==start && people[start]<=limit){
-              // System.out.println("only start increasing "+start);
-                count++;
-                start++;
-            }
-            
-        //    System.out.println("********");
-            
-            
+            } 
+            end--;
+            count++;
             
         }
+        
         
         return count;
         
