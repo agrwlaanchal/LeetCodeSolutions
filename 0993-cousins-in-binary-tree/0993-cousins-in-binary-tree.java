9f116bc914a26aa80ead27e1e51d63b9fe8382fc
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-05-08 21:45:05 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (79.35%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     int par_x = -1;
+        int par_y =-1;
+        int level_x =-1;
+        int level_y =-1;
+       
+    public boolean isCousins(TreeNode root, int x, int y) {
+        
+        //to solve this problem 
+        // we need 2 things 
+        // parent of each var here 
+        // and level of each var here. 
+         // so we can have 4 variables. 
+        
+        
+        traverse(root, x, y, 0);
+        if(root.val==x){
+            level_x=0;
+        }
+        
+        if(root.val==y){
+            level_y=0;
+        }
+        
+       if(par_x!=par_y && level_x==level_y){
+           return true;
+       }   
+        return false;
+        
+    }
+    
+    public void traverse(TreeNode root, int x, int y, int level){
+        
+        
+        if(root==null){
+            return; 
+        }
+        
+        if(root.left!=null && root.left.val==x || root.right!=null&& root.right.val==x){
+            par_x=root.val;
+            level_x = level+1;
+        }
+        
+        
+        if(root.left!=null && root.left.val==y || root.right!=null&& root.right.val==y){
+            par_y=root.val;
+            level_y = level+1;
+        }
+        
+        traverse(root.left, x, y, level+1);
+        traverse(root.right, x, y, level+1);
+        
+    }
+    
+}
