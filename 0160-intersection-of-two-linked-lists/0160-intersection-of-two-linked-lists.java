ae647dd0c7243939514410d8ada258724048b252
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-05-08 19:28:18 +0530

    Time: 1 ms (99.93%), Space: 48.4 MB (54.25%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -12,20 +12,48 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
      
-        while (head2 != null) {
-            ListNode temp = head1;
-            while (temp != null) {
-                // if both Nodes are same
-                if (temp == head2) {
-                    return head2;
-                }
-                temp = temp.next;
-            }
-            head2 = head2.next;
-        }
-        // If intersection is not present between the lists,
-        // return NULL.
+        ListNode ptr1 = head1;
+    ListNode ptr2 = head2;
+
+    // If any one of head is null i.e
+    // no Intersection Point
+    if (ptr1 == null || ptr2 == null) {
+
         return null;
+    }
+
+    // Traverse through the lists until they
+    // reach Intersection node
+    while (ptr1 != ptr2) {
+
+        ptr1 = ptr1.next;
+        ptr2 = ptr2.next;
+
+        // If at any node ptr1 meets ptr2, then it is
+        // intersection node.Return intersection node.
+
+        if (ptr1 == ptr2) {
+
+            return ptr1;
+        }
+        /* Once both of them go through reassigning,
+        they will be equidistant from the collision point.*/
+
+        // When ptr1 reaches the end of a list, then
+        // reassign it to the head2.
+        if (ptr1 == null) {
+
+            ptr1 = head2;
+        }
+        // When ptr2 reaches the end of a list, then
+        // redirect it to the head1.
+        if (ptr2 == null) {
+
+            ptr2 = head1;
+        }
+    }
+
+    return ptr1;
         
     }
 }
