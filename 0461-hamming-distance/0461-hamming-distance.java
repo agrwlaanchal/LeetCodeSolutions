0f089e3711ce84f58c959b8cddaad189ef646060
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-05-10 22:44:57 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (62.59%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,29 +1,14 @@
 class Solution {
     public int hammingDistance(int x, int y) {
        
-        String num1 = Integer.toBinaryString(x);
-        String num2 = Integer.toBinaryString(y);
-        
-        
-        while(num1.length()<num2.length()){
-            num1= "0"+num1;
+         int count=0;
+        while(x>0||y>0)
+        {
+            if(x%2!=y%2)count++;
+            x/=2;
+            y/=2;
         }
-        
-        while(num1.length()>num2.length()){
-            num2= "0"+num2;
-        }
-        int res =0;
-        for(int i=0;i<num1.length();i++){
-            
-            char c = num1.charAt(i);
-            char t = num2.charAt(i);
-            
-            if(c!=t){
-                res++;
-            }
-            
-        }
-        return res;
+        return count;
         
         
     }
