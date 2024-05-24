dd1480930f5093e6bc874a2a91c09273c5a6d612
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-05-24 18:25:34 +0530

    Time: 13 ms (17.80%), Space: 43.4 MB (26.27%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -7,29 +7,15 @@ class Solution {
             hmap.put(c, hmap.getOrDefault(c,0)+1);
         }
         
-        List<List<String>>  res = new ArrayList<>();
         List<String> subset = new ArrayList<>();
         int index =0;
-         calsub(words, subset,res, hmap, 0,score);
+         calsub(words, subset, hmap, 0,score);
     
-        //  return res;
-   
-        /*for(List<String> l : res){
-         //   System.out.println(l.toString());
-             int sum = 0; 
-            for(String str: l){
-               for(int i=0;i<str.length();i++){
-                   char c =str.charAt(i);
-                  // System.out.println("c is "+c);
-                   sum=sum+score[c-'a'];
-               }
-            }
-            max=Math.max(max,sum);
-        }*/
+      
         return max;
     }
     
-    public void calsub(String[]nums,List<String> subset ,  List<List<String>>  res, HashMap<Character, Integer> hmap, int index,int[] score){
+    public void calsub(String[]nums,List<String> subset , HashMap<Character, Integer> hmap, int index,int[] score){
         
         //res.add(new ArrayList<>(subset));
         int sum =0 ; 
@@ -55,7 +41,7 @@ class Solution {
             
             if(temp.equals(nums[i])){
                subset.add(nums[i]); 
-               calsub(nums, subset, res, hmap,i+1,score);
+               calsub(nums, subset, hmap,i+1,score);
                 subset.remove(subset.size()-1);
                 
             }
