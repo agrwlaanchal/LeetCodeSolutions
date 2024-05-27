ed0c8c682f01666ded689aeaa288bc52678bc45d
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-05-27 15:03:57 +0530

    Time: 43 ms (100.00%), Space: 81.4 MB (100.00%) - LeetHub

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
         
-        
-        
-      //  int []arr = new int[limit+1];
-       
-         HashMap<Integer, Integer> ballColors = new HashMap<>();
+        HashMap<Integer, Integer> ballColors = new HashMap<>();
         
         HashMap<Integer, Integer> hmap = new HashMap<>(); //this is hashmap of color and count 
         int[]res = new int[queries.length];
-     //   Arrays.fill(arr, 0);
        
-        hmap.put(queries[0][1], 1);
+       // hmap.put(queries[0][1], 1);
          
-        res[0]=hmap.size();
-        //arr[queries[0][0]]=queries[0][1];
-        ballColors.put(queries[0][0],queries[0][1] );
-        for(int i=1;i<queries.length;i++){
+       // res[0]=hmap.size();
+        //ballColors.put(queries[0][0],queries[0][1] );
+       
+        for(int i=0;i<queries.length;i++){
             
             int ball = queries[i][0];
             int color = queries[i][1];
@@ -30,14 +25,9 @@ class Solution {
                 }
             }
             
-            
-              //  arr[ball]=color;
                ballColors.put(ball, color);
                 hmap.put(color, hmap.getOrDefault(color,0)+1);
                 res[i]=hmap.size();
-                
-            
-  
             
         }
         
