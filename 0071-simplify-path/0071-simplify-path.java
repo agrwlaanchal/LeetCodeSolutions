4a706ca0d7a8bd94fcfbb497e8ca5f5d8289e8b0
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-05-27 20:49:41 +0530

    Time: 6 ms (35.69%), Space: 44.3 MB (26.75%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String simplifyPath(String path) {
+     
+        
+        String[]arr = path.split("/");
+        
+        Stack<String> st = new Stack<>();
+        
+        for(int i=0; i<arr.length;i++){
+            
+            String str = arr[i];
+       //     System.out.println("working on "+str);
+            if(str.length()<=0){
+                continue; 
+            }
+            if(str.equals(".")){
+                //do nothing 
+            }else if(str.equals("..")){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }else {
+             st.push(str);   
+            }
+            
+        }
+        
+        String[]res= new String[st.size()];
+        for(int i=st.size()-1;i>=0;i--){
+            String temp="/"+st.pop();
+         //   System.out.println(temp);
+            res[i]=temp;
+        }
+        
+        String result="";
+        for(int i=0;i<res.length;i++){
+            result=result+res[i];
+        }
+        if(result.length()<=0){
+            return "/";
+        }
+        return result;
+        
+        
+        
+    }
+}
