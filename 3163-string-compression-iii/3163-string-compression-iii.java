b2e1d3b6915e21dd90f7d59b95a48658c699594b
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-05-27 14:12:54 +0530

    Time: 18 ms (100.00%), Space: 46.1 MB (66.67%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.java
+++ 3163-string-compression-iii/3163-string-compression-iii.java
@@ -5,7 +5,7 @@ class Solution {
             return word; 
         }
        
-        String comp ="";
+        StringBuilder comp =new StringBuilder();
         
         int count=1;
         char ch = word.charAt(0);
@@ -18,17 +18,18 @@ class Solution {
                 count++;
                 index++;
             }else{
-                comp=comp+count+ch;
+                //comp=comp+count+ch;
+                comp.append(count).append(ch);
                 ch=word.charAt(index);
                 index++;
                 count=1;
             }
         }
         
-       comp=comp+count+ch;
-          
+       //comp=comp+count+ch;
+          comp.append(count).append(ch); 
         
-        return comp; 
+        return comp.toString(); 
         
     }
 }
