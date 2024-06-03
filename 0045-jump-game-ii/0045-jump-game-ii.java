6d6119b01aa2d3cbf55c6cde25803a9f96380deb
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-06-03 15:05:57 +0530

    Time: 299 ms (5.01%), Space: 44.4 MB (97.23%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+        
+          int []res = new int[nums.length];
+        Arrays.fill(res, Integer.MAX_VALUE);
+     
+       res[0]=0;  
+        
+      outer:  for(int i=1; i<nums.length;i++){
+            
+            //to reach at nums[i];
+            
+            for(int j=i-1; j>=0;j--){
+                
+                if(nums[j]+j>=i){
+                    //can reach at i 
+                    res[i] = Math.min(res[i], res[j]+1);
+                }
+                
+            }
+            
+        }
+        System.out.println(Arrays.toString(res));
+        return res[res.length-1];
+        
+    }
+}
