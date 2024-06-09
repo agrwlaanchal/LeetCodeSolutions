4529514414c1e20d3de6443c52d97cd5a2b154dd
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-06-09 20:10:27 +0530

    Time: 3 ms (100.00%), Space: 42.5 MB (15.46%) - LeetHub

--- 0284-peeking-iterator/0284-peeking-iterator.java
+++ 0284-peeking-iterator/0284-peeking-iterator.java
@@ -2,41 +2,41 @@
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
 class PeekingIterator implements Iterator<Integer> {
-    ArrayList<Integer> arr ; 
-    int index =0; 
+   Iterator<Integer> iterator;
+    Integer buffer ; 
 	public PeekingIterator(Iterator<Integer> iterator) {
 	    // initialize any member here.
-        arr = new ArrayList<>();
+/*        arr = new ArrayList<>();
         while (iterator.hasNext()) {
             arr.add(iterator.next());
-        }
+        }*/
+        this.iterator = iterator;
+        buffer = iterator.hasNext() ? iterator.next() : null;
 	    
 	}
 	
     // Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-        if(arr.size()>0){
-            return arr.get(index);
-        }
-        return -1;
+       
+      return buffer; 
 	}
 	
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-	    int num = arr.get(index);
-        index++;
+       
+        int num = buffer;
+        buffer = iterator.hasNext() ? iterator.next() : null;
         return num;
+	    
 	}
 	
 	@Override
 	public boolean hasNext() {
 	    
-        if(index<arr.size()){
-            return true;
-        }
-        return false; 
+       return buffer != null;
+        
         
 	}
 }
