cb2c0996456a9315586f71418353284a1c13e2dc
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-06-10 18:17:37 +0530

    Time: 2 ms (88.93%), Space: 41.1 MB (65.80%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        
+        int[]arr = heights.clone();
+        Arrays.sort(heights);
+        int res =0;
+        
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=arr[i]){
+                res++;
+            }
+        }
+        
+        return res; 
+    }
+}
