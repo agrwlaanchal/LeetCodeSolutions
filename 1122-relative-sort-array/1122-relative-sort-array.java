61b3a51ece9469420dbb53342d5248e0d2901ed6
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-06-11 18:28:17 +0530

    Time: 1 ms (73.84%), Space: 42.5 MB (6.92%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,31 +1,40 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
      
-        TreeMap<Integer, Integer> tmap = new TreeMap<>();
+     
+        int temp[] = new int[1001];
+        Arrays.fill(temp, 0);
+        
         for(int num:arr1){
-            tmap.put(num ,tmap.getOrDefault(num , 0)+1);
+            temp[num]++;
         }
+        
         int[]res = new int[arr1.length];
         int index =0; 
         for(int i=0;i<arr2.length;i++){
             
             int num = arr2[i];
-            int count = tmap.get(num);
+            int count = temp[num];
             for(int j=0;j<count;j++){
                 res[index++]=num;
             }
-           tmap.remove(num); 
+          temp[num]=0;
         }
         
-        for(Map.Entry<Integer,Integer>entry: tmap.entrySet()){
+        for(int i=0;i<temp.length;i++){
             
-            int count = entry.getValue();
-            int num = entry.getKey();
-             for(int j=0;j<count;j++){
-                res[index++]=num;
+            if(temp[i]>0){
+                
+                int count = temp[i];
+                for(int j=0;j<count;j++){
+                res[index++]=i;
+                 }
+                
             }
             
         }
+        
+        
         
         return res;
         
