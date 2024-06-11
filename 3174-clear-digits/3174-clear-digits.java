c64f08fdbd876bc3f9de8b1d0ae8bece8a238232
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-06-11 19:52:00 +0530

    Time: 2 ms (86.06%), Space: 42.3 MB (71.40%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.java
+++ 3174-clear-digits/3174-clear-digits.java
@@ -3,9 +3,9 @@ class Solution {
         
         Stack<Character> st = new Stack<>();
         
-        char[]arr = s.toCharArray();
-        for(char c:arr){
-            
+      //  char[]arr = s.toCharArray();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
             if(Character.isDigit(c)){
             
                 if(!st.isEmpty()){
