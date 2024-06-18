980cb3e4c70dad37181234b6f4acc8628d8488aa
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-06-18 14:05:03 +0530

    Time: 1159 ms (5.03%), Space: 48.5 MB (5.40%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -2,20 +2,19 @@ class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
  
        
-      //  int[]pro  = new int[profit.length];
         
         TreeMap<Integer,Integer> tmap = new TreeMap<>();
         for(int i=0; i<profit.length;i++){
             int prof = profit[i];
             int diff = difficulty[i];
-            //for this diff, what's the max profit  
+            if(tmap.containsKey(diff)){
+                continue;
+            }
             for(int j=0;j<profit.length;j++){
                 if(difficulty[j]<=diff && profit[j]>=prof){
                     prof = profit[j];
                 }
             }
-        //    pro[i]=prof;
-            
             tmap.put(diff, prof);
             
         }
