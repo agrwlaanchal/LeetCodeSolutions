69e6304900adbf3eb8b827696724b26fbd800a81
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-06-19 19:26:55 +0530

    Time: 781 ms (5.33%), Space: 65 MB (37.71%) - LeetHub

--- 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.java
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.java
@@ -1,34 +1,53 @@
 class Solution {
-    public long maximumTotalDamage(int[] power) {
-            Map<Integer, Long> frequency = new HashMap<>();
-        for (int p : power) {
-            frequency.put(p, frequency.getOrDefault(p, 0L) + 1);
+    public long maximumTotalDamage(int[] pow) { 
+        Arrays.sort(pow);
+     
+        TreeMap<Long, Long> tmap = new TreeMap<>();
+
+        for (int num : pow) {
+            tmap.put((long)num, tmap.getOrDefault((long)num, 0L) + 1L);
         }
         
-        List<Integer> keys = new ArrayList<>(frequency.keySet());
-        Collections.sort(keys);
-        int N = keys.size();
+     /*   TreeMap<Integer,Long> tmap = new TreeMap<>();
+       
+        for(int num:pow){
+            tmap.put(num, tmap.getOrDefault(num, 0L)+1);
+            
+        }*/
         
-        long[] dp = new long[N];
+        Set<Long> set = tmap.keySet();
         
-        for (int i = 0; i < N; i++) {
-            int currentKey = keys.get(i);
-            dp[i] = currentKey * frequency.get(currentKey);
-            for (int j = 1; j < 4; j++) {
-                if (i - j >= 0 && currentKey - keys.get(i - j) > 2) {
-                    dp[i] = Math.max(dp[i], dp[i - j] + currentKey * frequency.get(currentKey));
+        long[]power =new long[set.size()];
+        int index =0; 
+        for(long t: set){
+            power[index] = t;
+            index++;
+        }
+        long[]dp = new long[set.size()];
+        
+        dp[0] = tmap.get(power[0])*power[0];
+        long res = dp[0];
+        for(int i=1;i<power.length;i++){
+        //    System.out.println("power is "+power[i]);
+            int count=0;
+            int j=i-1;
+            long temp = 0;
+            while(j>=0&&count<4){
+                
+                if(power[j]==power[i]-2 || power[j]==power[i]-1){
+                    temp=Math.max(temp, dp[j]);
+                }else{
+                    temp=Math.max(temp, dp[j]+power[i]*tmap.get(power[i]));
                 }
+                j--;
+                count++;
             }
-            if (i > 0) {
-                dp[i] = Math.max(dp[i], dp[i - 1]);
-            }
+            temp = Math.max(temp, power[i]*tmap.get(power[i]));
+         //   System.out.println("temp is "+temp);
+            dp[i]=temp;
+            res=Math.max(dp[i],res);
         }
+        return res;
         
-        long maxDamage = 0;
-        for (long damage : dp) {
-            maxDamage = Math.max(maxDamage, damage);
-        }
-        
-        return maxDamage;
     }
 }
