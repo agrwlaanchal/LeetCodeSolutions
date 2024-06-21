c0fc2a0fe089281712559b17da7b60c60687096c
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-06-21 19:08:58 +0530

    Time: 4 ms (48.61%), Space: 46.9 MB (78.12%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -2,20 +2,16 @@ class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
      
         int n = customers.length;
-        int postsum[] = new int[n];
-        int presum[] = new int[n];
-        presum[0]=0;
-        if(grumpy[0]==0){
-            presum[0] = customers[0];
+        int res =0;
+        for(int i=0;i<minutes;i++){
+            res+=customers[i];
         }
+        //start to substract
+        //end to add. 
+        // 
         
-        for(int i=1;i<n;i++){
-            presum[i]=presum[i-1];
-            if(grumpy[i]==0){
-                presum[i]+=customers[i];
-            }
-        }
-        
+         int postsum[] = new int[n];
+       
         postsum[n-1]=0;
         if(grumpy[n-1]==0){
             postsum[n-1] = customers[n-1];
@@ -29,28 +25,32 @@ class Solution {
         }
         
         
+        
         int start =0;
-        int end = minutes-1;
-        int res =0; 
-        while(end<n){
-            int sum =0;
-            for(int i=start;i<=end;i++){
-                sum=sum+customers[i];
-            }
+        int end =-1;
+        if(minutes<n)
+         end=minutes;
+        else{
+        //    end = n-1;
+        return res;
+        }
+        int temp = res;
+        res=res+postsum[end];
+       
+        while(end+1<n){
             
-            if(start-1>=0){
-                sum=sum+presum[start-1];
-            }
-            if(end+1<n){
-                sum=sum+postsum[end+1];
-            }
+            temp=temp-(customers[start]*grumpy[start])+customers[end];
+            if(end+1<n)
+            res=Math.max(res, temp+postsum[end+1]);
             
-            res=Math.max(res, sum);
             start++;
             end++;
         }
-       
-        return res; 
+
+         temp=temp-(customers[start]*grumpy[start])+customers[end];
+         res=Math.max(res, temp);
+             return res;
+        
         
     }
 }
