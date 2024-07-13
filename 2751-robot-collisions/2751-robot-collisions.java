788f45508ecb01b0dcccfd3c19d491d06b3932c1
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-07-13 16:50:48 +0530

    Time: 65 ms (30.00%), Space: 61.4 MB (48.75%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.java
+++ 2751-robot-collisions/2751-robot-collisions.java
@@ -14,17 +14,14 @@ class Pair {
 class Solution {
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
      
-        
-        
-        
-        
-       int n = positions.length;
+      int n = positions.length;
         Pair[] robots = new Pair[n];
 
         for (int i = 0; i < n; i++) {
             robots[i] = new Pair(positions[i], healths[i], directions.charAt(i));
         }
 
+        // sort the array based on position
         Arrays.sort(robots, Comparator.comparingInt(r -> r.pos));
    
         
@@ -32,25 +29,21 @@ class Solution {
         Stack<Pair> st = new Stack<>();
         
         
-        // first left and last  right 
-        // keep checking and and then keep updating  
-        //  lets say we maintain left to right  
+        // robots collide if current is  left and previous is right
         
         for(int i=0; i<robots.length;i++){
             
             int p = robots[i].pos;
             char d = robots[i].dir;
-            int hel = robots[i].hel;
-            
+            int hel = robots[i].hel;    
             
             if(st.isEmpty()){
                 Pair pair = new Pair(p,hel,d);
                 st.push(pair);
             }else{
-                
-                
+                           
                 if(d=='L'){
-                    
+                  // compare with previous and if it's right    
                    while (!st.isEmpty() && st.peek().dir == 'R') {
                         Pair p1 = st.pop();
                         if (p1.hel == hel) {
@@ -76,38 +69,23 @@ class Solution {
             
         }
         
-        // get the result 
-      //  List<Integer> result = new ArrayList<>();
-        
-        //make a list of positions from stack and add in list
-        // travel through positions, is in list
-        // add to list and remove from list 1 
-        
-        
-       // ArrayList<Integer> temp = new ArrayList<>();
-        //ArrayList<Integer> temph = new ArrayList<>();
-      
+       //make map of result list robots position and health   
+        // since output is to be returned in same order as input 
         HashMap<Integer, Integer> hmap = new HashMap<>();
         
         while(!st.isEmpty()){
             Pair p = st.pop();
             hmap.put(p.pos, p.hel);
-           // temp.add(p.pos);
-            //temph.add(p.hel);
         }
         
+        // add to the result
         for(int pos: positions){
-            
             if(hmap.containsKey(pos)){
-               // int inde = temp.indexOf(pos);
                 res.add(hmap.get(pos));
             }
-            
-            
         }
         
         return res;
-        
         
     }
 }
