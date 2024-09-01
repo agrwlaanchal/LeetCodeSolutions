10284bb310e1943a735e08311f79e08aca0c9922
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-09-01 12:15:11 +0530

    Time: 3 ms (97.73%), Space: 55.5 MB (57.13%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -10,9 +10,9 @@ class Solution {
         int index =0; 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int num=original[index];
-                res[i][j]=num;
-                index++;
+                
+                res[i][j]=original[index++];
+                
             }
         }
         return res;
