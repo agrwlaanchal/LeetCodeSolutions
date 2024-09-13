d527a67fe91c54c2c1a570e805af8eaf7d63f894
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-09-13 11:48:16 +0530

    Time: 168 ms (15.93%), Space: 54.5 MB (34.96%) - LeetHub

---
+++ 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String lastNonEmptyString(String s) {
+        
+       HashMap<Character, Integer> hmap =new HashMap<>();
+        int max= 0 ;
+        
+        for(int i=0;i<s.length();i++){
+            
+            char c = s.charAt(i);
+            if(hmap.containsKey(c)){
+                hmap.put(c, hmap.get(c)+1);
+            }else{
+                hmap.put(c,1);
+            }
+            max=Math.max(max, hmap.get(c));
+            
+        }
+        
+        
+        HashMap<Character, Integer> temp =new HashMap<>();
+        
+        for(Map.Entry<Character, Integer> entry: hmap.entrySet()){
+            
+            if(entry.getValue()==max){
+                temp.put(entry.getKey(), 1);
+                //hmap.remove(entry.getKey());
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--){
+            
+            char c = s.charAt(i);
+            if(temp.containsKey(c)){
+                sb.append(c);
+                temp.remove(c);
+            }
+        }
+        
+        return sb.reverse().toString();
+        
+        
+    }
+}
