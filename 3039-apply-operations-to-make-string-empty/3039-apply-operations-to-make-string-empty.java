951bef9630bab0c770ec575e971f118d42ec2f41
agrwlaanchal <30493905+agrwlaanchal@users.noreply.github.com>
2024-09-13 11:50:28 +0530

    Time: 204 ms (6.64%), Space: 54.6 MB (34.07%) - LeetHub

--- 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.java
+++ 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.java
@@ -33,7 +33,7 @@ class Solution {
         for(int i=s.length()-1;i>=0;i--){
             
             char c = s.charAt(i);
-            if(hmap.containsKey(c) && hmap.get(c)==max &&!visited[c-'a']){
+            if(!visited[c-'a'] && hmap.containsKey(c) && hmap.get(c)==max ){
                 sb.append(c);
                 visited[c-'a']=true;
             }
